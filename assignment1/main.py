f2943eb1a7174bb4e8e0ebd4748232ffce04e587
Prarabdha Srivastava <sriprarabdha2004@gmail.com>
2025-08-29 20:04:35 +0530

    added parser for 1/main.py

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,28 +2,36 @@ from tasks.task1 import plot_hist
 from tasks.task2 import run_task_2
 from tasks.task3 import run_task_3
 from tasks.task4 import run_task_4
+from argparse import ArgumentParser
 
-def main():
 
-    # task 1
-    img_path = "ip_images/coins.png"
-    plot_hist(img_path, save_dir="op_images")
+def main(tasks:list[int]):
 
-    # task 2
-    img_path = "ip_images/coins.png"
-    run_task_2(input_path=img_path, save_dir="op_images" , profiling=True, debug = True)    
+    tasks_list = {
+        1: ["ip_images/coins.png" , plot_hist],
+        2: ["ip_images/coins.png" , run_task_2],
+        3: ["ip_images/sudoku.png" , run_task_3],
+        4: ["ip_images/quote.png" , run_task_4],
+    }
 
-    #task 3
-    img_path = "ip_images/sudoku.png"
-    run_task_3(input_path=img_path, save_dir="op_images")
+    for key in tasks:
+        tasks_list[key][1](tasks_list[key][0])
 
-    #task 4
-    img_path = "ip_images/quote.png"
-    run_task_4(image_path=img_path , save_dir="op_images")
 
 if __name__ == "__main__":
-    main()
-
+    parser = ArgumentParser("Parser")
+    parser.add_argument(
+        "--task",
+        type=int,
+        nargs="+",            
+        default=[1,2,3,4],         
+        help="Which task(s) do you want to run? Default is [0]."
+    )
+    args = parser.parse_args()
+    print(args.task)
+    
+    main(args.task)   
+    
 
 
 
