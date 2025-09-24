0d358fb4df487406adf8f64ea1e558441ce92f02
Prarabdha Srivastava <sriprarabdha2004@gmail.com>
2025-09-24 23:17:06 +0530

    added results

--- assignment_2/apps/exp3.cpp
+++ assignment_2/apps/exp3.cpp
@@ -10,14 +10,20 @@ void sharpen_experiment(const std::string& image_path,std::optional<std::string>
     auto img = imageproc::io::loadImage(image_path);
     imageproc::io::showImage("original image", img);
 
-    imageproc::BoxFilter bf(29);
-    auto blurred_img = bf.apply(img);
+    // imageproc::BoxFilter bf(19);
+    // auto blurred_img = bf.apply(img);
 
-    imageproc::io::showImage("original image", blurred_img);
-    imageproc::SharpenFilter sf(10, 229);
+    // imageproc::io::showImage("original image", blurred_img);
+    imageproc::SharpenFilter sf(0.8, 29);
 
-    auto sharp_img = sf.apply(blurred_img, 17);
-    imageproc::io::showImage("sharp image", img);
+    // auto sharp_img = sf.apply(blurred_img, 17);
+    auto sharp_img = sf.apply(img, 17);
+    if(save_dir.has_value()){
+        std::string s = *save_dir;
+        // imageproc::io::saveImage(s+"/exp_3_blurred.png" , blurred_img);
+        imageproc::io::saveImage(s+"/exp_3_sharp.png" , sharp_img);
+    }
+    imageproc::io::showImage("sharp image", sharp_img);
 
     
     }
