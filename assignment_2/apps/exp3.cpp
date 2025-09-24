e622170a5f9e1bb98306ddc94e9277490507e6f9
Prarabdha Srivastava <sriprarabdha2004@gmail.com>
2025-09-24 23:26:10 +0530

    added box + gaussian for exp 3

--- assignment_2/apps/exp3.cpp
+++ assignment_2/apps/exp3.cpp
@@ -14,18 +14,20 @@ void sharpen_experiment(const std::string& image_path,std::optional<std::string>
     // auto blurred_img = bf.apply(img);
 
     // imageproc::io::showImage("original image", blurred_img);
-    imageproc::SharpenFilter sf(0.8, 29);
+    imageproc::SharpenFilter sf(0.8, 5);
 
-    // auto sharp_img = sf.apply(blurred_img, 17);
-    auto sharp_img = sf.apply(img, 17);
+    auto sharp_img_box = sf.apply(img);
+    auto sharp_img_gaussian = sf.apply(img, 17);
+    std::cout<<"fuck";
     if(save_dir.has_value()){
         std::string s = *save_dir;
         // imageproc::io::saveImage(s+"/exp_3_blurred.png" , blurred_img);
-        imageproc::io::saveImage(s+"/exp_3_sharp.png" , sharp_img);
+        imageproc::io::saveImage(s+"/exp_3_sharp_box.png" , sharp_img_box);
+        imageproc::io::saveImage(s+"/exp_3_sharp_gaussian.png" , sharp_img_gaussian);
     }
-    imageproc::io::showImage("sharp image", sharp_img);
+    imageproc::io::showImage("sharp image box", sharp_img_box);
+    imageproc::io::showImage("sharp image gaussian", sharp_img_gaussian);
 
     
     }
 }
-
